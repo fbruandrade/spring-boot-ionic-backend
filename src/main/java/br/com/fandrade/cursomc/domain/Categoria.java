8a61a3b87cf751477965763f7bae6a469fd6386b
Felipe Brunelli de Andrade <fbruandrade@gmail.com>
2019-07-23 20:26:23 -0300

    Add Database H2 (testing) and automate database creation

--- src/main/java/br/com/fandrade/cursomc/domain/Categoria.java
+++ src/main/java/br/com/fandrade/cursomc/domain/Categoria.java
@@ -1,12 +1,19 @@
 package br.com.fandrade.cursomc.domain;
 
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.io.Serializable;
 import java.util.Objects;
 
+@Entity
 public class Categoria implements Serializable {
 
     private static final long serialVersionUID = 3409593257377956793L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String nome;
 
