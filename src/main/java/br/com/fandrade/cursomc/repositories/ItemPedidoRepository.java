586463033f92a79eaacf293539655cd9dfd50cb0
Felipe Brunelli de Andrade <fbruandrade@gmail.com>
2019-08-02 02:00:22 -0300

    Created ItemPedido and ItemPedidoPK

---
+++ src/main/java/br/com/fandrade/cursomc/repositories/ItemPedidoRepository.java
@@ -0,0 +1,9 @@
+package br.com.fandrade.cursomc.repositories;
+
+import br.com.fandrade.cursomc.domain.ItemPedido;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ItemPedidoRepository extends JpaRepository<ItemPedido, Integer> {
+}
